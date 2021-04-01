78dce0f1995b586478e9788117160507a1e50e96
Anu Kollon <68405755+anukollon@users.noreply.github.com>
2021-04-01 15:42:50 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What color is black box in a airplane?";
+        String choiceOne = "Orange";
+        String choiceTwo = "Black";
+        String choiceThree = "Pink";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceOne;
 
         // Write a print statement asking the question
         // Write a print statement giving the answer choices
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to AK Quiz!");
+        System.out.println("Select the correct answer");
+        System.out.println(question);
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
+        System.out.println("Type A or B or C");
+        String input = scanner.next();
+              
+        if(input.toLowerCase().equals(correctAnswer.toLowerCase())) {
+        	System.out.println("Congrats! You got it right.");
+        }
+        else {
+        	System.out.println("Incorrect answer. Correct answer is "+correctAnswer);
+        }
 
         // Have the user input an answer
         // Retrieve the user's input
